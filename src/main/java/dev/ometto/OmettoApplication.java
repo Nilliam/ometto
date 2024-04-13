b7de295242583104612c9ddb7a1b0101f8602dc3
Nilliam Cris Ometto <nilliamometto@gmail.com>
2024-04-13 16:53:56 -0300

    initial

---
+++ src/main/java/dev/ometto/OmettoApplication.java
@@ -0,0 +1,13 @@
+package dev.ometto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmettoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ometto/OmettoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ometto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
